bd463c5c6ed7ee0d94731016763c4d875e5c0841
alinafekamwendo <48163686+alinafekamwendo@users.noreply.github.com>
2022-03-19 03:52:24 +0200

    ussd in progress

--- server/src/controllers/Ussd/ussdController.js
+++ server/src/controllers/Ussd/ussdController.js
@@ -1,6 +1,6 @@
 
 const express = require("express");
-const ussd = express();
+const ussdController = express();
 const cors = require("cors");
 const swaggerUI=require("swagger-ui-express");
 const YAML=require('yamljs');
@@ -8,70 +8,61 @@ const bodyPrser=require('body-parser');
 const swaggerJSDocs=YAML.load("./swagger.yaml");
 const logger = require('morgan');
 const dotenv=require("dotenv").config();
-const dairy=require("./src/models/DairyData.json");
+const {Users}=require("../../models")
+//const dairy=require("./src/models/DairyData.json");
 
-ussd.use(express.json());
-ussd.use(cors());
-ussd.use(logger('dev'))
+ussdController.use(express.json());
+ussdController.use(cors());
+ussdController.use(logger('dev'))
+//
+// const str = '01-01-2020';
+
+// // get everything after first dash
+// const slug = str.substring(str.indexOf('-') + 1); // 01-2020
+
 //app.use(bodyPrser.json());
 //app.use(bodyPrser.urlencoded({extended:true}))
-ussd.use(express.urlencoded({extended:true}));
+ussdController.use(express.urlencoded({extended:true}));
 
-ussd.post('*', (req, res,next) => {
+ussdController.post('/', async (req, res,next) => {
+
+  let {sessionId,
+    serviceCode, 
+    phoneNumber, 
+    text} = req.body
+    let response;
+if(text==="")
+{
+ response='CON Enter your username'
+
+}
+if(text!=="")
+{
+  // let incoming=text.split('*');
+  // const username=incoming[0];
+  // const password=incoming[1];
+  // const user=Users.findOne({where:{username:username,password:password}});
+  // console.log(user);
+  console.log(`incomings ${incoming}`);
+response='CON enter your password'
+}
+
+// setTimeout(()=>{
+//   console.log(text);
+//   res.send(response);
+//   res.end();
+// },20000);
+   
+  });
+
+ussdController.get("/get",(req,res,next)=>{
   try {
-    let {sessionId, serviceCode, phoneNumber, text} = req.body
-    if (text == '') {
-      console.log(req.body);
-      // This is the first request. Note how we start the response with CON
-      let response = `CON WELCOME TO IWETA
-      1. CHICHEWA
-      2. ENGLISH`
-      res.send(response)
-      //console.log(`welcome text ${text}`);
-    } else if (text == '1') {
-      // Business logic for first level response
-      let response = `CON YAMBANI
-       Lowetsani nambala ya chinsinsi`
-      res.send(response)
-      //console.log(`chichewa text ${text}`);
-    } else if (text == '2') {
+    res.status(200).send("USSD RUNNING");
   
-        let response = `CON LETS START
-        Enter your ID`
-        res.send(response)
-       // console.log(`english text ${text}`);
-     
-      
-    
-      // Business logic for first level response
-      
-      
-    } else if (text == '1*1') {
-      // Business logic for first level response
-      // This is a terminal request. Note how we start the response with END
-      let response = `CON  Sakhani khola
-      1.Lilongwe khola
-      2.Chinamwali khola
-      3.Matawale`
-      res.send(response)
-      //console.log(`kusakha khola text ${text}`);
-    } else if (text == '1*2') {
-      // This is a second level response where the user selected 1 in the first instance
-      // This is a terminal request. Note how we start the response with END
-      let response = `CON Choose khola
-      1.Lilongwe
-      2.Chinamwali
-      3.Matawale`
-      res.send(response)
-      console.log(`choosing khola text ${text}`);
-    } else {
-     // console.log(text);
-      res.status(400).send('Bad request!')
-    }
   } catch (error) {
     next(error);
   }
-   
-  })
+ 
+})
 
-  export default ussd;
+  module.exports =ussdController;
