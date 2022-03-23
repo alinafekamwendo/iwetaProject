4736a35e1d121f93a3918a990a0bb322bc43739b
alinafekamwendo <bed-com-15-17@unima.ac.mw>
2022-03-23 15:47:26 +0200

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -3,26 +3,47 @@
 require('dotenv').config();
 const {DB, DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
 module.exports=
-{
+{  
   "development": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB,
-    "host": DB_HOST,
+    "username": "postgres",
+    "password": "1517",
+    "database": "alis",
+    "host": "localhost",
     "dialect": "postgres"
   },
   "test": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB,
-    "host": DB_HOST,
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "production": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB,
-    "host": DB_HOST,
+    "username": "postgres",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
     "dialect": "postgres"
-}
+  }
+//   "development": {
+//     "username": DB_USERNAME,
+//     "password": DB_PASSWORD,
+//     "database": DB,
+//     "host": DB_HOST,
+//     "dialect": "postgres"
+//   },
+//   "test": {
+//     "username": DB_USERNAME,
+//     "password": DB_PASSWORD,
+//     "database": DB,
+//     "host": DB_HOST,
+//     "dialect": "postgres"
+//   },
+//   "production": {
+//     "username": DB_USERNAME,
+//     "password": DB_PASSWORD,
+//     "database": DB,
+//     "host": DB_HOST,
+//     "dialect": "postgres"
+// }
 }
